ace0feec2f95867fa69d25cfca75e17a5de14332
JoseMilans <josemilans@gmail.com>
2023-06-15 09:03:34 -0700

    0. Positive anything is better than negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - assigns a random number to the variable n each time it is executed.
  *        The source code prints whether the number stored in the variable n is positive           or negative.
