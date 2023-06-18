8aa0a1dc776bd0100de4f3f1ac9170dd31411535
JoseMilans <josemilans@gmail.com>
2023-06-18 14:57:34 -0700

    Write a function that prints all natural numbers from n to 98, followed by a new line.

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,38 +1,61 @@
 #include "main.h"
+int _putchar(char c);
+/**
+ * print_number - Prints a number
+ * @n: The number to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
 /**
  * print_to_98 - Prints all natural numbers from n to 98.
  * @n: The starting number.
  */
-int _putchar(char c);
-
 void print_to_98(int n)
 {
-    while (1)
-    {
-        if (n < 0)
-        {
-            _putchar('-');
-            n = -n;
-        }
+	int i;
 
-        if (n / 100)
-            _putchar((n / 100) + '0');
-
-        if (n / 10)
-            _putchar((n / 10) % 10 + '0');
-
-        _putchar((n % 10) + '0');
-
-        if (n == 98)
-            break;
-
-        _putchar(','), _putchar(' ');
-
-        if (n < 98)
-            n++;
-        else
-            n--;
-    }
-
-    _putchar('\n');
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				print_number(i);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				print_number(i);
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				print_number(i);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				print_number(i);
+			}
+		}
+	}
+	_putchar('\n');
 }
