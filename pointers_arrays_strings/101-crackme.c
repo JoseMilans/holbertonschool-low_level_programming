7d54227005bff901a58428d152feb9b356b63ac7
JoseMilans <josemilans@gmail.com>
2023-06-27 19:16:33 -0700

    101-crackme.c 11. Create a program that generates random valid passwords for the program 101-crackme.

---
+++ pointers_arrays_strings/101-crackme.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define PASSWORD_LENGTH 15
+/**
+ * main - Generates a random valid password for the crackme program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	char password[PASSWORD_LENGTH + 1];
+
+	srand(time(NULL));
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		int randomValue;
+
+		do {
+			randomValue = rand() % 94 + 33;
+		} while (randomValue == 127);
+		password[i] = (char)randomValue;
+	}
+	password[PASSWORD_LENGTH] = '\0';
+	printf("%s\n", password);
+	return (0);
+}
