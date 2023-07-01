7ecde48fb370336fcb1049f656377056a5b7118e
JoseMilans <josemilans@gmail.com>
2023-07-01 15:53:08 -0700

    4-add.c 4 Write a program that adds positive numbers.

---
+++ argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+		if (atoi(argv[i]) == 0 && *argv[i] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
