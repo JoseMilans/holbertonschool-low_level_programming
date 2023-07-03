789f946b71a73e09c7d938612ec7449b5dc468b3
JoseMilans <josemilans@gmail.com>
2023-07-03 13:01:41 -0700

    100-is_palindrome.c 7. Write a function that returns 1 if a string is a palindrome and 0 if not.

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
-/**
- * _is_palindrome - Recursive helper function to check if a string is a
- *  palindrome
- * @s: The string to check
- * @start: The starting index of the string
- * @end: The ending index of the string
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
-int _is_palindrome(char *s, int start, int end)
-{
-	if (start >= end)
-		return (1);
-	if (s[start] != s[end])
-		return (0);
-	return (_is_palindrome(s, start + 1, end - 1));
-}
+int _is_palindrome(char *s, int start, int end);
 
 /**
  * is_palindrome - Checks if a string is a palindrome
@@ -27,9 +11,35 @@ int _is_palindrome(char *s, int start, int end)
 int is_palindrome(char *s)
 {
 	int length = 0;
-
+    /* Calculate the length of the string */
 	while (s[length] != '\0')
 		length++;
 	return (_is_palindrome(s, 0, length - 1));
 }
 
+/**
+ * _is_palindrome - Recursive helper function to check if a string is a
+ * palindrome
+ * @s: The string to check
+ * @start: The starting index of the string
+ * @end: The ending index of the string
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int _is_palindrome(char *s, int start, int end)
+{
+    /*
+	 * Base case: if the start index crosses or equals the end index, it's a
+	 * palindrome
+	 */
+	if (start >= end)
+		return (1);
+    /*
+	 * If the characters at start and end indices are different, it's not
+	 * a palindrome
+	 */
+	if (s[start] != s[end])
+		return (0);
+    /* Recursively check the substring by moving the start and end indices */
+	return (_is_palindrome(s, start + 1, end - 1));
+}
