93cf46e5496f9a36d4f48e186d9944619865d6ae
JoseMilans <josemilans@gmail.com>
2023-07-03 12:46:34 -0700

    100-is_palindrome.c 7. Write a function that returns 1 if a string is a palindrome and 0 if not.

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _is_palindrome - Recursive helper function to check if a string is a
+ *  palindrome
+ * @s: The string to check
+ * @start: The starting index of the string
+ * @end: The ending index of the string
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int _is_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (_is_palindrome(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: The string to check
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	return (_is_palindrome(s, 0, length - 1));
+}
+
