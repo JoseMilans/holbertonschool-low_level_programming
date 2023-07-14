d268a523a0ff8e683a066d3cbcdd554b23571b6a
JoseMilans <josemilans@gmail.com>
2023-07-13 18:17:37 -0700

    101-mul.c 5. Write a program that multiplies two positive numbers.

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,108 +1,95 @@
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-/**
- * multiply - Multiplies two positive numbers
- * @num1: The first number
- * @num2: The second number
- * Return: The result of multiplication as a string
- */
-char *multiply(char *num1, char *num2)
-{
-	int len1, len2, lenRes, i, j, prod, carry;
-	char *result, *newResult = NULL;
-
-	if (*num1 == '0' || *num2 == '0')
-	{
-		result = malloc(sizeof(char) * 2);
-		if (result == NULL)
-			return (NULL);
-		result[0] = '0';
-		result[1] = '\0';
-		return (result);
-	}
-	len1 = strlen(num1);
-	len2 = strlen(num2);
-	lenRes = len1 + len2;
-	result = malloc(sizeof(char) * (lenRes + 1));
-	newResult = result;
-	if (result == NULL)
-		return (NULL);
-	for (i = 0; i < lenRes; i++)
-		result[i] = '0';
-	result[lenRes] = '\0';
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			prod = (num1[i] - '0') * (num2[j] - '0') +
-				(result[i + j + 1] - '0') + carry;
-			carry = prod / 10;
-			result[i + j + 1] = (prod % 10) + '0';
-		}
-		result[i] += carry;
-	}
-	if (result[0] == '0')
-	{
-		newResult = malloc(sizeof(char) * (lenRes + 1));
-		if (newResult == NULL)
-		{
-			free(result);
-			return (result);
-		}
-		strcpy(newResult, result + 1);
-		free(result);
-		return (newResult);
-	}
-	return (result);
-}
-/**
- * isPositiveNumber - Checks if a string represents a positive number
- * @str: The string to check
- * Return: 1 if the string represents a positive number, 0 otherwise
- */
-int isPositiveNumber(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (!isdigit(str[i]))
-			return (0);
-	}
-	return (1);
-}
+#include <unistd.h>
 /**
  * main - Entry point
- * @argc: The number of commandline arguments
- * @argv: An array of commandline argument strings
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char **argv)
 {
 	char *num1, *num2, *result;
+	int len1, len2, lenRes, i;
 
 	if (argc != 3)
 	{
-		printf("Error\n");
+		printError();
 		return (1);
 	}
 	num1 = argv[1];
 	num2 = argv[2];
-	if (!isPositiveNumber(num1) || !isPositiveNumber(num2))
+	len1 = findLength(num1);
+	len2 = findLength(num2);
+	if (len1 == 0 || len2 == 0)
 	{
-		printf("Error\n");
+		printError();
 		return (1);
 	}
 	result = multiply(num1, num2);
-	if (result == NULL)
-	{
-		printf("Error: Memory allocation failed\n");
-		return (1);
-	}
-	printf("%s\n", result);
+	lenRes = findLength(result);
+	i = 0;
+	while (result[i] == '0' && i < lenRes - 1)
+		i++;
+	write(1, result + i, lenRes - i);
+	write(1, "\n", 1);
 	free(result);
 	return (0);
+}
+/**
+ * multiply - Multiply two numbers
+ * @num1: First number as a string
+ * @num2: Second number as a string
+ * Return: Pointer to the result as a string
+ */
+char *multiply(char *num1, char *num2)
+{
+	int len1, len2, lenRes, i, j, n1, n2, carry, product;
+	char *result;
+
+	len1 = findLength(num1);
+	len2 = findLength(num2);
+	lenRes = len1 + len2;
+	result = malloc(sizeof(char) * (lenRes + 1));
+	if (result == NULL)
+		printError();
+	for (i = 0; i < lenRes; i++)
+		result[i] = '0';
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		n1 = num1[i] - '0';
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			product = (n1 * n2) + (result[i + j + 1] - '0') + carry;
+			result[i + j + 1] = (product % 10) + '0';
+			carry = product / 10;
+		}
+		if (carry > 0)
+			result[i + j + 1] += carry;
+	}
+	return (result);
+}
+
+/**
+ * findLength - Calculate the length of a string
+ * @str: The input string
+ * Return: The length of the string
+ */
+int findLength(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * printError - Print error message and exit with status 1
+ */
+void printError(void)
+{
+	write(2, "Error\n", 6);
+	exit(98);
 }
