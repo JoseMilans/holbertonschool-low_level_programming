ed10f9a5f3189fbf9fb689a081e6398b996ae980
JoseMilans <josemilans@gmail.com>
2023-08-02 06:59:27 -0700

    Updated function declaration.

--- file_io/main.h
+++ file_io/main.h
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void close_files(int fd_from, int fd_to);
-void copy_content(int fd_from, int fd_to);
+void copy_content(int fd_from, int fd_to, char *file_from, char *file_to);
 void check_args(int argc, char *argv[]);
 
 #endif
