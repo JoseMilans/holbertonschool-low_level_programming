771693e257c41ad3d958539ef859cd26d593e8f6
JoseMilans <josemilans@gmail.com>
2023-08-01 20:49:07 -0700

    Updated function declaration.

--- file_io/main.h
+++ file_io/main.h
@@ -16,6 +16,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void close_files(int fd_from, int fd_to);
 void copy_content(int fd_from, int fd_to);
-void check_args(int argc);
+void check_args(int argc, char *argv[]);
 
 #endif
