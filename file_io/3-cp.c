5becff03f32220770dfbd08b2beb681cd29ef260
JoseMilans <josemilans@gmail.com>
2023-08-01 20:49:34 -0700

    A program that copies the content of a file to another file.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,11 +7,15 @@
 void close_files(int fd_from, int fd_to)
 {
 	if (close(fd_from) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-	exit(100);
+		exit(100);
+	}
 	if (close(fd_to) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-	exit(100);
+		exit(100);
+	}
 }
 /**
  * copy_content - copies content from one file to another
@@ -27,23 +31,35 @@ void copy_content(int fd_from, int fd_to)
 	{
 		write_status = write(fd_to, buffer, bytes_read);
 		if (write_status != bytes_read)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd_to);
-		exit(99);
+			close_files(fd_from, fd_to);
+			exit(99);
+		}
 	}
 	if (bytes_read == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd_from);
-	exit(98);
+		close_files(fd_from, fd_to);
+		exit(98);
+	}
 }
 /**
  * check_args - checks if the correct number of arguments is passed
  * @argc: number of arguments
+ * @argv: the arguments
  */
-void check_args(int argc)
+void check_args(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
+	if (argv[2][0] == '\0')
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to \n");
+		exit(99);
 	}
 }
 /**
@@ -57,17 +73,19 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to;
 	mode_t file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	check_args(argc);
+	check_args(argc, argv);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+		exit(98);
+	}
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, file_perm);
 	if (fd_to == -1)
 	{
 		close(fd_from);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	exit(99);
+		exit(99);
 	}
 	copy_content(fd_from, fd_to);
 	close_files(fd_from, fd_to);
